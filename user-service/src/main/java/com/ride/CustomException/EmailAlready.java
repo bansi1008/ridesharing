87c4c89fc74a38480b7cbf0aad8837b890ee5c81
Bansi Dobariya <bansidobariya2510@gmail.com>
2026-04-25 13:55:08 +0100

    global Exception Handler added

--- user-service/src/main/java/com/ride/CustomException/EmailAlready.java
+++
@@ -1,8 +0,0 @@
-package com.ride.CustomException;
-
-public class EmailAlready extends RuntimeException {
-    public EmailAlready(String email) {
-        super("Email already exists: " + email);
-    }
-    
-}
--- user-service/src/main/java/com/ride/Model/Role.java
+++ user-service/src/main/java/com/ride/Model/Role.java
@@ -1,7 +1,19 @@
 package com.ride.Model;
 
-public enum  Role {
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum Role {
     USER,
     ADMIN,
-    DRIVER
+    DRIVER;
+
+    @JsonCreator
+    public static Role from(String value) {
+        for (Role role : Role.values()) {
+            if (role.name().equalsIgnoreCase(value)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Invalid role: " + value);
+    }
 }
